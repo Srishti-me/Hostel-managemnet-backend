9b350f76b91289a37392a125490f30e888f4c049
Srishti Yadav <134631519+Srishti-me@users.noreply.github.com>
2024-06-30 02:22:39 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "database-1.chagskcoeoyo.us-east-2.rds.amazonaws.com", // Example: mydbinstance.123456789012.us-east-1.rds.amazonaws.com
+  host: "hostelsrishh.chagskcoeoyo.us-east-2.rds.amazonaws.com",
   user: "admin",
   password: "Srishdb#321",
-  database: "database-1",
+  database: "mydatabase",
   port: 3306, 
   connectionLimit: 10 
 });
