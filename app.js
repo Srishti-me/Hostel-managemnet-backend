0818caa71c869f00071cbb09c554e759e0658bb1
Srishti Yadav <134631519+Srishti-me@users.noreply.github.com>
2024-06-30 02:48:58 +0530

    Updated cors url

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500'
+  origin: ['http://127.0.0.1:5500', 'https://srishti-me.github.io']
 }));
 
 // Routes
